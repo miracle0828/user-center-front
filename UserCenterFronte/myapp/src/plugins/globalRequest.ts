908e53f9609ad95df827aac9959fbf97753b38f3
wu tao <wutao6422@163.com>
2024-06-07 23:23:10 +0800

    add 线上request请求

--- UserCenterFronte/myapp/src/plugins/globalRequest.ts
+++ UserCenterFronte/myapp/src/plugins/globalRequest.ts
@@ -12,7 +12,8 @@ import {stringify} from "querystring";
  */
 const request = extend({
   credentials: 'include', // 默认请求是否带上cookie
-  prefix: process.env.NODE_ENV === 'production' ? 'http://user-backend.code-nav.cn' : undefined
+  // prefix: process.env.NODE_ENV === 'production' ? 'http://user-backend.code-nav.cn' : undefined
+  prefix: process.env.NODE_ENV === 'production' ? 'http://101.200.185.143' : undefined
   // requestType: 'form',
 });
 
